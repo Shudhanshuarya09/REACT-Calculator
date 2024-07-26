5d79b8922146450393174d02e1c9adafed330424
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-07-26 12:15:14 +0530

    Intialization

---
+++ Calculator/src/App.jsx
@@ -0,0 +1,17 @@
+
+import styles from './App.module.css';
+
+function App() {
+  
+
+  return (
+    <div>
+      <input type='text'/>
+      <div className='buttons-container'>
+        <buttons>C</buttons>
+      </div>
+    </div>
+  )
+}
+
+export default App
