4c9b7eb69bf7e3ff3d8881da27874f2953b5b819
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-07-27 23:30:50 +0530

    CSS added

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -1,17 +1,20 @@
-
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
 function App() {
-  
-
   return (
     <div className={styles.calculator}>
-      <input className={styles.display} type='text'/>
-      <div className='buttons-container'>
-        <buttons>C</buttons>
+      <input className={styles.display} type="text" />
+      <div className={styles.buttonsContainer}>
+        <buttons className={styles.button}>C</buttons>
+        <buttons className={styles.button}>1</buttons>
+        <buttons className={styles.button}>2</buttons>
+        <buttons className={styles.button}>3</buttons>
+        <buttons className={styles.button}>4</buttons>
+        <buttons className={styles.button}>5</buttons>
+        <buttons className={styles.button}>6</buttons>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
